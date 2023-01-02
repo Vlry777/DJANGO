a79375c6e804346c7e506c0be498c50f0d66263c
camila f.ulloa <camiferulloa@gmail.com>
2023-01-02 14:06:36 -0300

    Django proyect de 0

---
+++ products/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Products (models.Model):
+    name = models.CharField(max_length= 100)
+    price = models.FloatField()
+    stock = models.BooleanField()
+
+class Category(models.Model):
+    name = models.CharField(max_length=50,unique= True)
---
+++ python_34650/views.py
@@ -0,0 +1,29 @@
+
+from django.http import HttpResponse
+from datetime import datetime
+from django.shortcuts import render
+
+def hola_mundo(request):
+    return HttpResponse('hola mundo')
+
+def otra_mas(request):
+    return HttpResponse('si otra mas')
+
+def fecha_actual(request):
+    hoy = datetime.now().date()
+    return HttpResponse(f'la fecha de hoy es {hoy}')
+
+def vista_con_edad(request,edad):
+    return HttpResponse(f'esto funciona? la edad es {edad}?')
+
+def vista_con_template(request):
+    return render(request, 'template.html', context={})
+
+def saludo_desde_template(request):
+    context = {
+        'nombre': 'Camila',
+        'edad': 18,
+        'usa_lentes': False,
+        'lista_frutas': ['banana','manzana', 'pera', 'anana']
+    }
+    return render(request, 'saludo.html', context= context)
