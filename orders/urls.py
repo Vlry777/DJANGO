1aaade3210b8debabbf2364e4977d677890b6245
Vlry777 <camiferulloa@gmail.com>
2023-01-05 16:27:02 -0300

    orders actualizacion

--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from orders.views import list_orders,create_orders
+
 urlpatterns = [
-    
+    path('list-orders/',list_orders),
+    path('create-orders/',create_orders),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from orders.models import Order
+
+def list_orders(request):
+    orders = Order.objects.all()
+    context = {
+        'orders' : orders,
+    }
+    return render(request, 'orders/list_orders.html', context=context)
+
+def create_orders(request):
+    Order.objects.create(client= 'Lara', product='Curso de PYTHON',payment_method= 'Card')
+    return HttpResponse('Order create')
+
