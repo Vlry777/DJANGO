9fbe13aa5ee22fc077d43db33f1b0154a476dd22
Vlry777 <camiferulloa@gmail.com>
2023-01-07 13:50:58 -0300

    admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import Products
+
+# admin.site.register(Products) #codigo para subir  y editar en base de datos
+
+@admin.register(Products) #edicion de admin django
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','price','stock')
+    list_filter = ('stock',)
+    search_fields = ('name',)
+
